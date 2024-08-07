096e4dd6a5705db09e6872dd54bf75ad66a33e3c
szoftveres <szoftveres@email.com>
2024-08-06 22:05:33 -0700

    more segments (bss, data, rodata) linked

--- big/dload/service/main.c
+++ big/dload/service/main.c
@@ -2,28 +2,39 @@
 
 int increment (int a);
 
+hostapi_t* host;
 
 
 
 /* By convention the entry point is the first function */
 
-int _start (hostapi_t* host) {
+int _start (hostapi_t* h) {
     int b;
+    int* p;
 
-    host->pull_data((char*)&b, sizeof(b));
-
-    b = increment(b);
+    host = h;
 
     host->conlog("Service dynamically linked and running\n");
 
-    host->push_data((char*)&b, sizeof(b));
+    host->pull_data((char*)&b, sizeof(b));
+
+    p = (int*) host->malloc(sizeof(int));
+    if (!p) {
+        host->conlog("Malloc failed\n");
+    }
+    *p = increment(b);
+
+    host->push_data((char*)p, sizeof(int));
+    host->free(p);
+
+    host->conlog("Service terminating\n");
 
     return 0;
 }
 
 
 int increment (int a) {
-    return a+1;
+    return a + 1;
 }
 
 
